3830e22ad3a890d665912a411c7f0f48a56a8873
poorya1001 <poorya3684.minaei@gmail.com>
2026-09-14 19:26:11 +0330

    Refactor properties for ColorHex and DelayText

--- Models/ClickPoint.cs
+++ Models/ClickPoint.cs
@@ -9,15 +9,28 @@ public enum CheckMode
 public class ClickPoint
 {
     public int Number { get; set; }
+
     public int X { get; set; }
+
     public int Y { get; set; }
+
+    // فاصله زمانی از این نقطه تا نقطه بعدی.
+    // برای آخرین نقطه، فاصله زمانی تا نقطه اولِ دور بعدی است.
     public int DelayMs { get; set; } = 1000;
+
     public byte R { get; set; }
+
     public byte G { get; set; }
+
     public byte B { get; set; }
+
     public int Tolerance { get; set; } = 10;
+
     public CheckMode CheckMode { get; set; } = CheckMode.None;
 
-    public string ColorHex => $"#{R:X2}{G:X2}{B:X2}";
-    public string DelayText => $"{DelayMs / 1000.0:0.###}s";
+    public string ColorHex =>
+        $"#{R:X2}{G:X2}{B:X2}";
+
+    public string DelayText =>
+        $"{DelayMs / 1000.0:0.###}s";
 }
